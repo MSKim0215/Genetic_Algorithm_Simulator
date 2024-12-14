f9e835411142c7f670212122366ec2bd455b0e35
MSKim0215 <rlaalstjqu05@gmail.com>
2024-12-14 18:28:52 +0900

    DNA 구현

---
+++ Assets/Scripts/DNA.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+
+namespace MinSeob.Genetic
+{
+    public class DNA<T>
+    {
+        public T[] Genes { private set; get; }             // 유전자
+        public float Fitness { private set; get; }         // 적합도
+
+        private Random random;
+        private Func<T> getRandomGene;
+        private Func<float, int> fitnessFunction;
+
+        public DNA(int size, Random random, Func<T> getRandomGene, Func<float, int> fitnessFunction, bool isInitGene = true)
+        {
+            Genes = new T[size];
+
+            this.random = random;
+            this.getRandomGene = getRandomGene;
+            this.fitnessFunction = fitnessFunction;
+
+            if(isInitGene)
+            {
+                for(int i = 0; i < Genes.Length; i++)
+                {
+                    Genes[i] = getRandomGene();
+                }
+            }
+        }
+
+        public float CalcuateFitness(int index)
+        {
+            return Fitness = fitnessFunction(index);
+        }
+
+        public void CrossOver(DNA<T> otherParent)
+        {
+            var child = new DNA<T>(Genes.Length, random, getRandomGene, fitnessFunction, isInitGene: false);
+
+            for(int i = 0; i < Genes.Length; i++)
+            {
+                child.Genes[i] = random.NextDouble() < 0.5f ? Genes[i] : otherParent.Genes[i];
+            }
+        }
+
+        public void Mutate(float mutateRatio)
+        {
+            for(int i = 0; i < Genes.Length; i++)
+            {
+                if(random.NextDouble() < mutateRatio)
+                {
+                    // TODO: Mutate Do SomeThing
+                }
+            }
+        }
+    }
+}
